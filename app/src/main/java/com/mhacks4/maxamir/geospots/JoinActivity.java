839372c4fa785772d92fffd3a0cb941988b54880
Amir Alavi <s.amir.alavi@gmail.com>
2014-09-06 01:25:59 -0400

    Added amirs files.

---
+++ app/src/main/java/com/mhacks4/maxamir/geospots/JoinActivity.java
@@ -0,0 +1,49 @@
+package com.mhacks4.maxamir.geospots;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.content.Intent;
+import android.widget.EditText;
+import com.mhacks4.maxamir.geospots.R;
+
+public class JoinActivity extends Activity {
+
+    public final static String EXTRA_MESSAGE = "com.mhacks4.maxamir.geospots.MESSAGE";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_join);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.join, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_settings) {
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public void sendURL(View view) {
+        Intent intent = new Intent(this, PlayActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        intent.putExtra(EXTRA_MESSAGE, message);
+        startActivity(intent);
+    }
+}
